f364075545099cf97cdd5280142add54a8342f03
Philipp Jenni <philippjenni@bluemail.ch>
2016-07-25 23:32:02 +0200

    https://trello.com/c/lWBETLBw Refactoring: Move Conversion of PDFs to SingleSongToImageConverterJob - Moved code

--- src/main/scala/ch/scotty/Main.scala
+++ src/main/scala/ch/scotty/Main.scala
@@ -1,28 +1,14 @@
 package ch.scotty
 
-import java.io.File
-import java.io.FileInputStream
-import java.nio.file.Paths
+import ch.scotty.generatedschema.Tables
+import ch.scotty.job.JobRunner
+import ch.scotty.job.json.{JobDefinitions, JobParser}
+import slick.driver.MySQLDriver.api._
+import slick.lifted.TableQuery
 
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
-import org.apache.pdfbox.pdmodel.PDDocument
-import org.apache.pdfbox.rendering.ImageType
-import org.apache.pdfbox.rendering.PDFRenderer
-import org.apache.pdfbox.tools.imageio.ImageIOUtil
-import ch.scotty.generatedschema.Tables
-import slick.driver.MySQLDriver.api._
-import slick.lifted.Query
-import slick.lifted.Rep
-import slick.lifted.TableQuery
-import ch.scotty.converter.LiedSourcePdfFileFinder
-import ch.scotty.converter.LiedWithData
-import ch.scotty.converter.SongnumberFinder
-import ch.scotty.converter.Songnumber
-import ch.scotty.job.JobRunner
-import ch.scotty.job.json.{JobDefinitions, JobParser, SingleSongToImageConverterJobConfiguration}
-import net.java.truecommons.io.Loan._
 
 object Main {
   val jobsPath : String = "jobs.json"
@@ -32,50 +18,7 @@ object Main {
     val jobDefinitions: JobDefinitions = JobParser.parseJobJson(readJsonFile)
     println("Running jobs...")
     JobRunner.runAllJobs(jobDefinitions)
-    val sw = Stopwatch.time("Converting all PDFs") {
-
-      val jobs : Seq[SingleSongToImageConverterJobConfiguration] = jobDefinitions.singleSongToImageConverterJob.get
-      jobs.par.foreach { aJob =>
-        //      jobs.foreach { aJob =>
-        val liedIdToFetch = aJob.songId
-        println(s"Going to read ${liedIdToFetch}...")
-        val liedData = LiedSourcePdfFileFinder.findFile(liedIdToFetch)
-        val songnumbers = SongnumberFinder.findSongnumbers(liedIdToFetch);
-        convertPdfBlobToImage(liedData, songnumbers)
-      }
-    }
-    println(s"Finished: ${sw}")
     Db.db.close()
-  }
-
-  private def convertPdfBlobToImage(liedWithData: LiedWithData, songnumbers: Seq[Songnumber]) = {
-    try {
-      val binaryStream = liedWithData.data.getBinaryStream
-      loan(PDDocument.load(binaryStream)).to { doc =>
-        //val doc = PDDocument.load(binaryStream);
-        val renderer = new PDFRenderer(doc)
-        val listOfImage = for (i <- 0 until doc.getNumberOfPages()) yield {
-          println(s"Exporting '${liedWithData.titel}' page " + (i + 1))
-          val bim = renderer.renderImageWithDPI(i, 90, ImageType.RGB)
-          val filename = buildFilename(liedWithData, songnumbers, i);
-          val path = Paths.get("data", filename).toString()
-          ImageIOUtil.writeImage(bim, path, 0);
-        }
-      }
-    } catch {
-      case e: Exception => {
-        System.err.println("Error while exporting " + liedWithData + ". Song is skipped. Error: " + e)
-
-      }
-    }
-  }
-
-  private def buildFilename(liedWithData: LiedWithData, songnumbers: Seq[Songnumber], sheetnumber: Integer): String = {
-    val titelWithOnlyAllowedCharacters = liedWithData.titel.replaceAll("[^a-zA-Z0-9äöüÄÖÜ .]", "")
-    val songnumberString = songnumbers.map { x => x.mnemonic + x.liednr }.mkString("_")
-
-    val filename = liedWithData.liedId + "-" + sheetnumber + "-" + songnumberString + "-" + titelWithOnlyAllowedCharacters + "-" + ".png"
-    filename
   }
 
   def readJsonFile(): String = {
--- src/main/scala/ch/scotty/job/JobRunner.scala
+++ src/main/scala/ch/scotty/job/JobRunner.scala
@@ -1,11 +1,16 @@
 package ch.scotty.job
 
+import ch.scotty.Stopwatch
 import ch.scotty.job.json.{JobConfiguration, JobDefinitions, JobParser}
 
 object JobRunner {
   def runAllJobs(jobDefinitions: JobDefinitions): Unit = {
-    AllSongToImageConverterJob.runIfJobsDefined(jobDefinitions)
-    SingleSongToImageConverterJob.runIfJobsDefined(jobDefinitions)
+    val sw = Stopwatch.time("All Jobs") {
+      AllSongToImageConverterJob.runIfJobsDefined(jobDefinitions)
+      SingleSongToImageConverterJob.runIfJobsDefined(jobDefinitions)
+    }
+    println
+    println(s"All jobs have been executed. Duration: ${sw}")
   }
 
 }
