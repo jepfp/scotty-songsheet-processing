9511f15ac09b8db670f641aa653d8660def2f50f
Philipp Jenni <pje@scaling.ch>
2020-08-15 23:38:13 +0200

    https://trello.com/c/5e41bddb69ce5133c675ef6d Toc entry must be individual per user! - removes functionality, that would add the tags with the band/group names for which this song was shared for that user because this is different for each user

--- src/main/scala/ch/scotty/converter/songanize/LiedWithDataBuilder.scala
+++ src/main/scala/ch/scotty/converter/songanize/LiedWithDataBuilder.scala
@@ -1,35 +1,28 @@
 package ch.scotty.converter.songanize
 
-import ch.scotty.converter.songanize.effect.SongForUser
 import ch.scotty.converter.{FileType, LiedWithData, SourceSystem}
 
-import scala.util.{Failure, Success, Try}
+import scala.util.{Success, Try}
 
 
 private class LiedWithDataBuilder {
 
   private final val baseSftpDir = "/private/files/"
 
-  def build(song: SonganizeSong, groupForUserList: Set[SongForUser])(remoteFileLoader: RemoteFileLoader): Try[LiedWithData] = {
+  def build(song: SonganizeSong)(remoteFileLoader: RemoteFileLoader): Try[LiedWithData] = {
     remoteFileLoader.retrieveFile(baseSftpDir + song.pathOnFileSystem + "/" + song.filenameOnFileSystem)
       .flatMap(blob => {
-        groupForUserList.find(_.songId == song.id) match {
-          case Some(groupForUser) =>
-            Success(LiedWithData(
-              sourceSystem = SourceSystem.Songanize,
-              songId = song.id,
-              title = Option(song.title).filter(_.trim.nonEmpty).getOrElse(song.filename),
-              tonality = Option(song.tonality).filter(_.trim.nonEmpty),
-              tags = List(groupForUser.groupName), //the group (or "Private" for own songs) which shared this song with that user is added as a tag.
-              createdAt = song.timestamp.toLocalDateTime,
-              updatedAt = song.lastChange.toLocalDateTime,
-              data = blob,
-              fileType = FileType(song.fileType)
-            ))
-
-          case None =>
-            Failure(new IllegalStateException(s"Could not find songId=${song.id} in groupForUserList=$groupForUserList"))
-        }
+        Success(LiedWithData(
+          sourceSystem = SourceSystem.Songanize,
+          songId = song.id,
+          title = Option(song.title).filter(_.trim.nonEmpty).getOrElse(song.filename),
+          tonality = Option(song.tonality).filter(_.trim.nonEmpty),
+          tags = List.empty,
+          createdAt = song.timestamp.toLocalDateTime,
+          updatedAt = song.lastChange.toLocalDateTime,
+          data = blob,
+          fileType = FileType(song.fileType)
+        ))
       })
 
   }
--- src/main/scala/ch/scotty/converter/songanize/SonganizeConverter.scala
+++ src/main/scala/ch/scotty/converter/songanize/SonganizeConverter.scala
@@ -2,7 +2,6 @@ package ch.scotty.converter.songanize
 
 import ch.scotty.converter.ConverterExporter
 import ch.scotty.converter.effect.{ExportPathResolverAndCreator, TableOfContentsDTOs}
-import ch.scotty.converter.songanize.effect.SongForUser
 import com.typesafe.scalalogging.Logger
 
 import scala.util.{Failure, Try}
@@ -11,19 +10,17 @@ import scala.util.{Failure, Try}
 class SonganizeConverter(exportPathResolverAndCreator: ExportPathResolverAndCreator = new ExportPathResolverAndCreator()) {
   val logger = Logger(classOf[SonganizeConverter])
 
-  def convertSong(songInfo: SonganizeSong, songForUserList: Set[SongForUser])(remoteFileLoader: RemoteFileLoader): Try[TableOfContentsDTOs.Song] = {
-    Try {
-      val conversionResult: Try[TableOfContentsDTOs.Song] = for (
-        liedData <- (new LiedWithDataBuilder()).build(songInfo, songForUserList)(remoteFileLoader);
-        r <- (new ConverterExporter(exportPathResolverAndCreator)).convertAndExport(liedData, Seq.empty)
-      ) yield r
+  def convertSong(songInfo: SonganizeSong)(remoteFileLoader: RemoteFileLoader): Try[TableOfContentsDTOs.Song] = {
+    val conversionResult: Try[TableOfContentsDTOs.Song] = for (
+      liedData <- (new LiedWithDataBuilder()).build(songInfo)(remoteFileLoader);
+      r <- (new ConverterExporter(exportPathResolverAndCreator)).convertAndExport(liedData, Seq.empty)
+    ) yield r
 
-      conversionResult recoverWith {
-        case t: Throwable =>
-          val m = s"Failure while converting songanize song. songInfo=$songInfo"
-          Failure(new Exception(m, t))
-      }
-    }.flatten
+    conversionResult recoverWith {
+      case t: Throwable =>
+        val m = s"Failure while converting songanize song. songInfo=$songInfo"
+        Failure(new Exception(m, t))
+    }
   }
 
 }
