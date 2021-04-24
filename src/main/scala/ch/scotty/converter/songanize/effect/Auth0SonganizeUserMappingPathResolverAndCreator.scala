effa328ce8133fc1ccce077fb46a2fb8e321dadf
Philipp Jenni <pje@scaling.ch>
2021-04-25 00:16:05 +0200

    https://trello.com/c/6004538158d7a8451c27afb4 Qualität und Grösse der konvertierten Bildern aus den PDFs zu schlecht bzw. zu gross - fixes wrong config paths

--- src/main/scala/ch/scotty/converter/songanize/effect/Auth0SonganizeUserMappingPathResolverAndCreator.scala
+++ src/main/scala/ch/scotty/converter/songanize/effect/Auth0SonganizeUserMappingPathResolverAndCreator.scala
@@ -1,22 +1,16 @@
 package ch.scotty.converter.songanize.effect
 
-import java.nio.file.{Files, Paths}
-
-import ch.scotty.converter.SourceSystem
-import com.typesafe.config.{Config, ConfigFactory}
+import ch.scotty.converter.{SongsheetConfig, SourceSystem}
 import com.typesafe.scalalogging.Logger
 
-private[effect] class Auth0SonganizeUserMappingPathResolverAndCreator(conf: Config) {
+import java.nio.file.{Files, Paths}
 
-  val config = conf.getConfig("converter")
+private[effect] class Auth0SonganizeUserMappingPathResolverAndCreator(config: SongsheetConfig = SongsheetConfig.get()) {
+
   val logger = Logger(classOf[Auth0SonganizeUserMappingPathResolverAndCreator])
 
-  def this() {
-    this(ConfigFactory.load())
-  }
-
   def resolve(): String = {
-    val basePath = config.getString("exportBaseDir")
+    val basePath = config.exportBaseDir
     Files.createDirectories(Paths.get(basePath, SourceSystem.Songanize.getIdentifier, "auth0SonganizeUserMapping"))
     val path = Paths.get(basePath, SourceSystem.Songanize.getIdentifier, "auth0SonganizeUserMapping", "mapping.json").toAbsolutePath.toString
     logger.debug(s"Output path: $path")
--- src/main/scala/ch/scotty/converter/songanize/effect/SongForUserListExportPathResolverAndCreator.scala
+++ src/main/scala/ch/scotty/converter/songanize/effect/SongForUserListExportPathResolverAndCreator.scala
@@ -1,22 +1,16 @@
 package ch.scotty.converter.songanize.effect
 
-import java.nio.file.{Files, Paths}
-
-import ch.scotty.converter.SourceSystem
-import com.typesafe.config.{Config, ConfigFactory}
+import ch.scotty.converter.{SongsheetConfig, SourceSystem}
 import com.typesafe.scalalogging.Logger
 
-private[songanize] class SongForUserListExportPathResolverAndCreator(conf: Config) {
+import java.nio.file.{Files, Paths}
 
-  val config = conf.getConfig("converter")
+private[songanize] class SongForUserListExportPathResolverAndCreator(config: SongsheetConfig = SongsheetConfig.get()) {
+
   val logger = Logger(classOf[SongForUserListExportPathResolverAndCreator])
 
-  def this() {
-    this(ConfigFactory.load())
-  }
-
   def resolve(userId: Long): String = {
-    val basePath = config.getString("exportBaseDir")
+    val basePath = config.exportBaseDir
     Files.createDirectories(Paths.get(basePath, SourceSystem.Songanize.getIdentifier, "songForUserList"))
     val path = Paths.get(basePath, SourceSystem.Songanize.getIdentifier, "songForUserList", userId.toString + ".json").toAbsolutePath.toString
     logger.debug(s"Output path: $path")
